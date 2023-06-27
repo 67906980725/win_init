dbf2275f04e987358acfdf9df0f614985f8cba1a
v <zh1024@zohomail.cn>
2023-06-27 19:02:58 +0800

    update

--- windows/free_file_sync.ps1
+++ windows/free_file_sync.ps1
@@ -4,5 +4,5 @@ $ver="12.3"
 $f_name="FreeFileSync_${ver}_Windows_Setup.exe"
 
 # Invoke-WebRequest https://freefilesync.org/download/$f_name -OutFile $home\Downloads\$f_name
-Invoke-WebRequest $GITHUB_PROXY/https://github.com/hkneptune/FreeFileSync/releases/download/$ver/v$f_name -OutFile $home\Downloads\$f_name
+Invoke-WebRequest ${GITHUB_PROXY}https://github.com/hkneptune/FreeFileSync/releases/download/$ver/v$f_name -OutFile $home\Downloads\$f_name
 Start-Process -FilePath "$home\Downloads\$f_name"
