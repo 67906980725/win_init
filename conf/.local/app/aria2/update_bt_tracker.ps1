9460fdd90297a61dac297149a24977e1742bd061
v <zh1024@zohomail.cn>
2023-07-13 12:56:24 +0800

    update

---
+++ conf/.local/app/aria2/update_bt_tracker.ps1
@@ -0,0 +1,30 @@
+$GITHUB_PROXY = $GITHUB_PROXY ?? "https://cors.isteed.cc/"
+$PATH_SEPARATOR = [System.IO.Path]::DirectorySeparatorChar
+$DOWN_PATH = $DOWN_PATH ?? "${home}${PATH_SEPARATOR}Downloads"
+
+function dw_git {
+  param (
+    [string] $url,
+    [string] $f_name
+  )
+  $l = "${GITHUB_PROXY}$url"
+  Invoke-WebRequest $l -OutFile $DOWN_PATH/$f_name
+}
+function dw_git_extract {
+  param (
+    [string] $url,
+    [string] $f_name,
+    [string] $dir_name
+  )
+  dw_git -url $url -f_name $f_name
+  Expand-Archive -Force -Path $DOWN_PATH/$f_name -DestinationPath $DOWN_PATH/$dir_name
+  Remove-Item $DOWN_PATH/$f_name
+}
+
+function update_bt_tracker {
+  dw_git_extract -url "https://github.com/ngosang/trackerslist/archive/refs/heads/master.zip" -f_name "trackerslist.zip" -dir_name ""
+  $tks = (Get-Content "$DOWN_PATH/trackerslist-master/trackers_best.txt") | Where-Object { $_ -match '\S' }
+  $tks = $tks -join ","
+  (Get-Content "$PSScriptRoot/aria2.conf") | Foreach-Object { $_ -replace '^bt-tracker=.*', "bt-tracker=$tks" } | Set-Content "$PSScriptRoot/aria2.conf"
+}
+update_bt_tracker
