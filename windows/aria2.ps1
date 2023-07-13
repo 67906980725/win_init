2941c6a07af3a5073a27a1ea68c3ff88d73d5ca9
v <zh1024@zohomail.cn>
2023-07-13 12:55:52 +0800

    update

--- windows/aria2.ps1
+++ windows/aria2.ps1
@@ -2,27 +2,44 @@
 
 $install_dir = "$I_APP_PATH${PATH_SEPARATOR}aria2"
 
-# require admin
-function auto_start {
-
-  "
-  `$process = Get-Process -Name 'aria2c' -ErrorAction SilentlyContinue
-  if (!`$process) {
-      Start-Process -FilePath ""$install_dir${PATH_SEPARATOR}aria2c.exe"" -ArgumentList ""--conf-path=$install_dir${PATH_SEPARATOR}aria2.conf"", ""--input-file=$install_dir${PATH_SEPARATOR}aria2.session"", ""--save-session=$install_dir${PATH_SEPARATOR}aria2.session""
-  }
-  " | Out-File -FilePath $install_dir/run.ps1
-  $trigger0 = New-ScheduledTaskTrigger -AtStartup 
-  $trigger1 = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
-  $trigger2 = New-ScheduledTaskTrigger -Daily -At 00:00 -DaysInterval 1
-  $trigger3 = New-ScheduledTaskTrigger -Daily -At 00:30 -DaysInterval 1
-  $trigger4 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionDuration (New-TimeSpan -Minutes 30) -RepetitionInterval (New-TimeSpan -Minutes 30)
-  # $action = New-ScheduledTaskAction -Execute "$install_dir${PATH_SEPARATOR}aria2c.exe" -Argument "--conf-path=$install_dir${PATH_SEPARATOR}aria2.conf --input-file=$install_dir${PATH_SEPARATOR}aria2.session --save-session=$install_dir${PATH_SEPARATOR}aria2.session"
-  $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File $install_dir${PATH_SEPARATOR}run.ps1"
-  Register-ScheduledTask -TaskName "aria2" -Trigger $trigger0, $trigger1, $trigger2, $trigger3, $trigger4 -Action $action
-  powershell.exe
-}
 
 function install_aria2 {
+  install aria2.aria2
+
+  New-Item $install_dir -ItemType Directory -Force -ErrorAction SilentlyContinue
+  "" | Out-File $install_dir/aria2.session -NoNewline
+  cp_conf ".local/app/aria2/aria2.conf"
+  (Get-Content "$install_dir/aria2.conf") | Foreach-Object { $_ -replace '^dir=.*', "dir=$DOWN_PATH"; } | Set-Content "$install_dir/aria2.conf"
+
+  cp_conf ".local/app/aria2/update_bt_tracker.ps1"
+  pwsh $install_dir/update_bt_tracker.ps1
+}
+
+function auto_start {
+  $l_file = "$install_dir\aria2c.lnk"
+  
+  $l = (New-Object -ComObject WScript.Shell).CreateShortcut($l_file)
+  $l.WorkingDirectory = $install_dir
+  $l.TargetPath = "aria2c.exe"
+  $l.Arguments = "--conf-path=aria2.conf"
+  $l.Save()
+
+  Copy-Item -Path $l_file -Destination "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" -Force  
+}
+
+function install_aria_ng {
+  $ng_ver = "1.3.6"
+  $ng_f_name = "AriaNg-$ng_ver-AllInOne.zip"
+  dw_git_extract -url "https://github.com/mayswind/AriaNg/releases/download/$ng_ver/$ng_f_name" -f_name "$ng_f_name" -dir_name "aria_ng"
+
+  Move-Item -Path $DOWN_PATH/aria_ng -Destination $I_APP_PATH -Force
+  Rename-Item -Path $I_APP_PATH/aria_ng/index.html -NewName aria_ng.html -Force
+
+  # 也可以用浏览器扩展接管aria2
+  # https://microsoftedge.microsoft.com/addons/detail/aria2-explorer/jjfgljkjddpcpfapejfkelkbjbehagbh?hl=zh-CN  
+}
+
+function install_aria2_bak {
   $ver = "1.36.0"
   $win64_dir = "aria2-$ver-win-64bit-build1"
   $win64_zip = "$win64_dir.zip"
@@ -40,24 +57,9 @@ function install_aria2 {
   (Get-Content "$install_dir/aria2.conf") | Foreach-Object { $_ -replace '^dir=.*', "dir=$DOWN_PATH"; } | Set-Content "$install_dir/aria2.conf"
 }
 
-function install_aria_ng {
-  $ng_ver = "1.3.6"
-  $ng_f_name = "AriaNg-$ng_ver-AllInOne.zip"
-  Invoke-WebRequest ${GITHUB_PROXY}https://github.com/mayswind/AriaNg/releases/download/$ng_ver/$ng_f_name -OutFile $DOWN_PATH/$ng_f_name
-  Expand-Archive -Force -Path $DOWN_PATH/$ng_f_name -DestinationPath $install_dir
-  Rename-Item -Path $install_dir/index.html -NewName aria_ng.html -Force
-}
-
-function update_bt_tracker {
-  dw_git_extract -url "https://github.com/ngosang/trackerslist/archive/refs/heads/master.zip" -f_name "trackerslist.zip" -dir_name ""
-  $tks = (Get-Content "$DOWN_PATH/trackerslist-master/trackers_best.txt") | Where-Object { $_ -match '\S' }
-  $tks = $tks -join ","
-  (Get-Content "$install_dir/aria2.conf") | Foreach-Object { $_ -replace '^bt-tracker=.*', "bt-tracker=$tks" } | Set-Content "$install_dir/aria2.conf"
-}
 
 install_aria2
-install_aria_ng
-update_bt_tracker
 auto_start
+install_aria_ng
 
 # https://www.cnblogs.com/alphaprime/p/15781824.html
