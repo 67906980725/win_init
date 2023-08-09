1f9beaa4d5f1b9e32ce8d8f88d1ad4f1a3835876
v <zh1024@zohomail.cn>
2023-08-09 12:34:35 +0800

    update

--- conf/Documents/PowerShell/util/mp4s_to_mp3.ps1
+++ conf/Documents/PowerShell/util/mp4s_to_mp3.ps1
@@ -1,20 +1,55 @@
 param(
-  [string]$dir
+  [string]$t, # 要转换的文件或文件夹
+  [string]$o # 生成的文件或文件夹
 )
 
-function mp4_to_mp3($file) {
-  $parent_dir = Split-Path -parent $file
-  $out_dir = "${parent_dir}_mp3"
-  New-Item "$out_dir" -ItemType Directory -ErrorAction SilentlyContinue
-  
-  $filename = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
-  $outputFile = Join-Path -Path $out_dir -ChildPath "${filename}.mp3"
-  $command = "ffmpeg -i ""$($file.FullName)"" ""$outputFile"""
+function mp4_to_mp3($file, $out) {
+  $command = "ffmpeg -i ""$($(Get-Item $file).FullName)"" ""$out"""
+  "执行命令: $command"
   Invoke-Expression $command
 }
 
-
-$mp4Files = Get-ChildItem -Path $dir -Filter "*.mp4" -File -Recurse
-foreach ($file in $mp4Files) {
-  mp4_to_mp3 $file $out_dir
+function get_mp3_name_by_file($file) {
+  $file_name = [System.IO.Path]::GetFileNameWithoutExtension($file)
+  "$file_name.mp3"
 }
+
+function trans_dir($t, $o) {
+  foreach ($file in $(Get-ChildItem -Path $t)) {
+    $file_name = [System.IO.Path]::GetFileNameWithoutExtension($file)
+    if ($file.PSIsContainer) {
+      $new_o = "${o}/${file_name}"
+      New-Item $new_o -ItemType Directory -ErrorAction SilentlyContinue
+      trans_dir $file "${new_o}"
+    }
+    else {
+      trans_file $file "$o"
+    }
+  }
+}
+
+function trans_file($t, $o) {
+  mp4_to_mp3 $t "${o}/$(get_mp3_name_by_file $t)"
+}
+
+function trans_item($t, $o) {
+  if ($(Get-Item $t).PSIsContainer) {
+    New-Item $o -ItemType Directory -ErrorAction SilentlyContinue
+    trans_dir $t $o
+  }
+  else {
+    mp4_to_mp3 $t $o
+  }
+}
+
+
+if (!$o) {
+  if ($(Get-Item $t).PSIsContainer) {
+    $o = "$($(Get-Item $t).Name)_mp3" 
+  }
+  else {
+    $o = "$(Get-Location)/$(get_mp3_name_by_file $t)"
+  }
+}
+
+trans_item $t $o
