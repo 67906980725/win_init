8b07270acb988a712090697fd8c762580fe0373d
v <zh1024@zohomail.cn>
2023-10-28 20:32:22 +0800

    update

--- windows/rust.ps1
+++ windows/rust.ps1
@@ -1,5 +1,9 @@
 . ./util.ps1
 
+# 安装 C++生成工具 https://blog.csdn.net/weixin_39842528/article/details/123448326
+# https://visualstudio.microsoft.com/zh-hans/visual-cpp-build-tools/
+# 只选择 "使用C++的桌面开发", 右侧安装详细信息下可选内容全选
+
 install JetBrains.IntelliJIDEA.Community
 install Rustlang.Rustup
 install Rustlang.Rust.MSVC
